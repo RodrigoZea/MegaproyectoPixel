70c791751899e15e50d8a517bc57b1890880dc73
RodrigoZea <zearodrigo37@gmail.com>
2021-07-30 12:02:32 -0600

    Cambiando todo a display por medio de rendertexture

---
+++ MegaproyectoPixel/Assets/Scripts/UITestingGM.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITestingGM : MonoBehaviour
+{
+    public Canvas canvas3DInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeOut();
+    }
+
+    void fadeOut() {
+        foreach(Transform child in transform) {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
