42b87ee10bccc596242c871b5082c1c1aeb5778e
val17102 <val17102@uvg.edu.gt>
2021-08-04 16:40:35 -0600

    Primera persona, arreglando movimiento, intertable interface y puerta

---
+++ MegaproyectoPixel/Assets/Miguel Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    private bool isOpen = false;
+    [SerializeField]
+    private float speed = 0.5f;
+    [SerializeField]
+    private Animator animator;
+    // Start is called before the first frame update
+
+    public void OnInteract(string condition){
+        isOpen = !isOpen;
+        if (isOpen){
+            animator.SetBool("isOpen", true);
+        } else {
+            animator.SetBool("isOpen", false);
+
+        }
+    }
+}
--- MegaproyectoPixel/Assets/Miguel Assets/Scripts/Weapon.cs
+++ MegaproyectoPixel/Assets/Miguel Assets/Scripts/Weapon.cs
@@ -6,13 +6,15 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField]
     private Transform raycastOrigin;
+    [SerializeField]
+    private Transform raycastDestiny;
 
     private Ray ray;
     private RaycastHit hitInfo;
     public void StartFiring() {
         Debug.Log("Fire");
         ray.origin = raycastOrigin.position;
-        ray.direction = raycastOrigin.forward;
+        ray.direction = raycastDestiny.position - raycastOrigin.position;
         if(Physics.Raycast(ray, out hitInfo)){
             Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1.0f);
             Debug.Log("Hit");
