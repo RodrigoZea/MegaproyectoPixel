52961d64b248618336a13d56dc3e01455fcdd48a
RodrigoZea <zearodrigo37@gmail.com>
2021-08-04 23:29:32 -0600

    no toy seguro que tanto adelante jeje pero seguramente arregle cosas en el display, y se empezo el inventario

--- MegaproyectoPixel/Assets/Rodrigo Assets/Scripts/UITestingGM.cs
+++ MegaproyectoPixel/Assets/Rodrigo Assets/Scripts/UITestingGM.cs
@@ -9,8 +9,13 @@ public class UITestingGM : MonoBehaviour
     public GameObject healthBar;
     public GameObject spookBar;
     public GameObject player;
+    public GameObject inventory;
     [SerializeField]
     private float idleTimer;
+    [SerializeField]
+    private int sizeWidth;
+    [SerializeField]
+    private int sizeHeight;
     public bool canvasFadeable;
     private float canvasFadeTimer;
     private bool canvasVisible = false;
@@ -18,6 +23,11 @@ public class UITestingGM : MonoBehaviour
     private ParticleSystem bloodDrops;
     private ParticleSystem  spiritDrops;
     private InputAction moveAction;
+    // Inventory
+    private Item[] itemList = new Item[20];
+
+    public GameObject Inventory3D;
+    public GameObject InventoryGroupCanvas;
 
     // Start is called before the first frame update
     void Start()
@@ -103,6 +113,24 @@ public class UITestingGM : MonoBehaviour
         if (updatedSpookScale.y <= 1.1f && updatedSpookScale.y >= -0.1f) {
             spookBar.transform.localScale = updatedSpookScale;
         }
+    }
+
+    // -------------------------------------------------------------------------------------------------------------------------------
+    public void showInventory() {
+        fadeGroup.gameObject.SetActive(false);
+        canvasVisible = false;
+
+        Inventory3D.gameObject.SetActive(true);
+        InventoryGroupCanvas.gameObject.SetActive(true);
+    }
+
+    void hideInventory() {
+        Inventory3D.gameObject.SetActive(false);
+        InventoryGroupCanvas.gameObject.SetActive(false);
+    }
+
+    void displayItems() {
+
     }
     
 }
