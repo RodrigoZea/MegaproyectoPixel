090d69ff2dfbcaa0e2a705bd46390421d514202f
val17102 <val17102@uvg.edu.gt>
2021-08-06 17:09:16 -0600

    Agregando Recoil, Arreglando movimiento primera persona

--- MegaproyectoPixel/Assets/Miguel Assets/Scripts/Weapon.cs
+++ MegaproyectoPixel/Assets/Miguel Assets/Scripts/Weapon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Weapon : MonoBehaviour
 {
@@ -8,10 +9,29 @@ public class Weapon : MonoBehaviour
     private Transform raycastOrigin;
     [SerializeField]
     private Transform raycastDestiny;
-
+    [SerializeField]
+    private float recoveryTime = 0.8f;
+    [SerializeField]
+    private Cinemachine.CinemachineVirtualCamera aimCamera;
+    [SerializeField]
+    private float verticaRecoil, duration;
+    private float time = 0;
     private Ray ray;
     private RaycastHit hitInfo;
-    public void StartFiring() {
+    private bool firing = false;
+    private bool fireAvailable = true;
+
+    void Update() {
+        if (fireAvailable && firing){
+            Fire();
+        }
+        if (time > 0){
+            aimCamera.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.Value -= verticaRecoil * Time.deltaTime;
+            time -= Time.deltaTime;
+        }
+    }
+
+    private void Fire(){
         Debug.Log("Fire");
         ray.origin = raycastOrigin.position;
         ray.direction = raycastDestiny.position - raycastOrigin.position;
@@ -19,9 +39,21 @@ public class Weapon : MonoBehaviour
             Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1.0f);
             Debug.Log("Hit");
         }
+        time = duration;
+        StartCoroutine(recovery(recoveryTime));
+    }
+
+    public void StartFiring() {
+        firing = true;
     }
 
     public void StopFiring(){
+        firing = false;
+    }
 
+    IEnumerator recovery(float time){
+        fireAvailable = false;
+        yield return new WaitForSeconds(time);
+        fireAvailable = true;
     }
 }
