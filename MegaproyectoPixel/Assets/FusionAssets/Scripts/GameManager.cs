0f910dd7b0558fff55edcc10ec74865b5ed56d61
RodrigoZea <zearodrigo37@gmail.com>
2021-09-26 16:12:45 -0600

    adding gamemanager

---
+++ MegaproyectoPixel/Assets/FusionAssets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum GameState {
+    PLAYING, PAUSED, NONE
+}
+
+public class GameManager : MonoBehaviour
+{
+    public CharacterControl playerInfo;
+    public PlayerInput playerInput;
+
+    private float playerHealth;
+    private float playerInsanity;
+    private int playerAmmo;
+    private GameState gameState;
+
+    //Singleton
+    public static GameManager Instance { get; private set; }
+
+    void Awake() {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }    
+    }
+
+
+    private void Start() {
+        gameState = GameState.PLAYING;
+        //playerInput.actions.Enable();
+    }
+
+    private void Update() {
+        
+    }
+}
