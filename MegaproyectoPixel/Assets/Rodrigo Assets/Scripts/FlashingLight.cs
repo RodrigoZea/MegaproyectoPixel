4376e136b44c83c750c63aa96f47e88762f010d0
RodrigoZea <zearodrigo37@gmail.com>
2021-10-19 17:28:40 -0600

    luces parpadeantes start

---
+++ MegaproyectoPixel/Assets/Rodrigo Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+    // Params for light
+    public int flashTimer;
+    public Material dimLight;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
