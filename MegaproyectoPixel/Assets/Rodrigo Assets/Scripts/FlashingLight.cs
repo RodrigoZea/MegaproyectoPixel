ce5c651443bb7bb6a53b9d0469294e758784be5a
RodrigoZea <zearodrigo37@gmail.com>
2021-10-19 19:41:03 -0600

    script de flashing lights funcional

--- MegaproyectoPixel/Assets/Rodrigo Assets/Scripts/FlashingLight.cs
+++ MegaproyectoPixel/Assets/Rodrigo Assets/Scripts/FlashingLight.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class FlashingLight : MonoBehaviour
 {
     // Params for light
-    public int flashTimer;
+    public float flashTimeLimit;
     public Material dimLight;
+    private Light lampLight;
+    private MeshRenderer meshRenderer;
+    private float flashTimer;
+    private bool initialFlash = true;
+    private Material initialMaterial;
+
     void Start()
     {
-        
+        lampLight = GetComponentInChildren<Light>();
+        meshRenderer =  transform.GetChild(0).GetComponent<MeshRenderer>();
+        initialMaterial = meshRenderer.material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        flashTimer += Time.deltaTime;
+        if (flashTimer >= flashTimeLimit) {
+            FlashLight();
+        }
+    }
+
+    void FlashLight() {
+        if (initialFlash) {
+            lampLight.enabled = false;
+            meshRenderer.material = dimLight;
+            initialFlash = false;
+        } else {
+            lampLight.enabled = true;
+            meshRenderer.material = initialMaterial;
+            initialFlash = true;            
+        }
+
+        flashTimer = 0;
     }
 }
